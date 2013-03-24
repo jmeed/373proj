d424e18f99a660e4cf7705a2420b244ba810174b
David Jackson <davijack@umich.edu>
2013-03-24 16:43:10 -0400

    Added NXP Test LED & Motor code

---
+++ Xpresso/Test LED_Motor/blinky/Debug/driver/i2cslave.d
@@ -0,0 +1,10 @@
+driver/i2cslave.d: ../driver/i2cslave.c ../config/driver_config.h \
+ ../cmsis/LPC11xx.h ../cmsis/core_cm0.h ../config/system_LPC11xx.h
+
+../config/driver_config.h:
+
+../cmsis/LPC11xx.h:
+
+../cmsis/core_cm0.h:
+
+../config/system_LPC11xx.h:
---
+++ Xpresso/Test LED_Motor/blinky/Debug/driver/wdt.d
@@ -0,0 +1,13 @@
+driver/wdt.d: ../driver/wdt.c ../config/driver_config.h \
+ ../cmsis/LPC11xx.h ../cmsis/core_cm0.h ../config/system_LPC11xx.h \
+ ../driver/wdt.h
+
+../config/driver_config.h:
+
+../cmsis/LPC11xx.h:
+
+../cmsis/core_cm0.h:
+
+../config/system_LPC11xx.h:
+
+../driver/wdt.h:
