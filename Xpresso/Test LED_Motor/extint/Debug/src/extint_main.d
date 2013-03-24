f1b8d719cfda873e60bb83b0a64e739685f967f6
David Jackson <davijack@umich.edu>
2013-03-24 17:23:15 -0400

    Adjusted NXP test. Interrupts known unworking.

---
+++ Xpresso/Test LED_Motor/extint/Debug/src/extint_main.d
@@ -0,0 +1,15 @@
+src/extint_main.d: ../src/extint_main.c ../config/driver_config.h \
+ ../cmsis/LPC11xx.h ../cmsis/core_cm0.h ../config/system_LPC11xx.h \
+ ../config/target_config.h ../driver/gpio.h
+
+../config/driver_config.h:
+
+../cmsis/LPC11xx.h:
+
+../cmsis/core_cm0.h:
+
+../config/system_LPC11xx.h:
+
+../config/target_config.h:
+
+../driver/gpio.h:
