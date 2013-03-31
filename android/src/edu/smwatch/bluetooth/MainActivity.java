f354eb4a45cd28ad9777f67b298e586f33e0ee2b
Filippos Theodorakis <ftheo@umich.edu>
2013-03-30 23:20:31 -0400

    Android app now seraches for available devices

--- android/src/edu/smwatch/bluetooth/MainActivity.java
+++ android/src/edu/smwatch/bluetooth/MainActivity.java
@@ -2,14 +2,51 @@ package edu.smwatch.bluetooth;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	public static final int REQUEST_ENABLE_BT = 1001;
+	
+	public static final int SELECT_DEVICE_BT = 1002;
+	
+	BluetoothAdapter mBluetoothAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			// TODO: Show a big warning sign and an error
+		    // Device does not support Bluetooth
+			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+			alertDialog.setTitle("No Bluetooth");
+			alertDialog.setMessage("Your device does not appear to have a bluetooth module");
+			alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+			   public void onClick(DialogInterface dialog, int which) {
+				   dialog.dismiss();
+			   }
+			});
+			// Set the Icon for the Dialog
+			//alertDialog.setIcon(R.drawable.icon);
+			alertDialog.show();
+		}
+		
+		// Enable Bluetoth
+		
+		if (!mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		} else {
+			bluetoot_is_enabled();
+		}
 	}
 
 	@Override
@@ -18,5 +55,33 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+			case REQUEST_ENABLE_BT: {
+				if (!mBluetoothAdapter.isEnabled()) {
+					// TODO: Show the same error screen but with a Bluetooth inactive message
+				} else {
+					bluetoot_is_enabled();
+				}
+				break;
+			}
+			default: {
+				super.onActivityResult(requestCode, resultCode, data);
+				break;
+			}
+		}
+	}
 
+	
+	public void bluetoot_is_enabled() {
+		startSearchDeviceIntent();
+	}
+
+	 
+	private void startSearchDeviceIntent() {
+		Intent serverIntent = new Intent(this, DeviceListActivity.class);
+		startActivityForResult(serverIntent, SELECT_DEVICE_BT);
+	}
 }
