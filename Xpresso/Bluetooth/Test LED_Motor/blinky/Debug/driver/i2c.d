4d956e8832c07ad4c6f4ba7e10e6187b5afec502
Filippos Theodorakis <ftheo@umich.edu>
2013-03-31 16:55:13 -0400

    Added a copy of the led test to start working on the bluetooth on the NXP instead of the Smartfusion

---
+++ Xpresso/Bluetooth/Test LED_Motor/blinky/Debug/driver/i2c.d
@@ -0,0 +1,10 @@
+driver/i2c.d: ../driver/i2c.c ../config/driver_config.h \
+ ../cmsis/LPC11xx.h ../cmsis/core_cm0.h ../config/system_LPC11xx.h
+
+../config/driver_config.h:
+
+../cmsis/LPC11xx.h:
+
+../cmsis/core_cm0.h:
+
+../config/system_LPC11xx.h:
---
+++ Xpresso/Bluetooth/Test LED_Motor/blinky/Debug/driver/ssp.d
@@ -0,0 +1,10 @@
+driver/ssp.d: ../driver/ssp.c ../config/driver_config.h \
+ ../cmsis/LPC11xx.h ../cmsis/core_cm0.h ../config/system_LPC11xx.h
+
+../config/driver_config.h:
+
+../cmsis/LPC11xx.h:
+
+../cmsis/core_cm0.h:
+
+../config/system_LPC11xx.h:
