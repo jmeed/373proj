5c39e123e594116fe882926ad19963117a24636b
Filippos Theodorakis <ftheo@umich.edu>
2013-04-05 07:25:15 -0400

    I WILL BREAK THIS STUPID I2C UART BOARDgit add .git add .

--- Xpresso/Bluetooth/Test LED_Motor/i2c/src/i2c_main.c
+++ Xpresso/Bluetooth/Test LED_Motor/i2c/src/i2c_main.c
@@ -28,6 +28,13 @@
 #define WAADR 0x90
 #define RAADR 0x91
 
+#define DIVL 0x00
+#define DIVM 0x08
+
+#define EFR_ENABLE_CTS 1 << 7
+#define EFR_ENABLE_RTS 1 << 6
+#define EFR_ENABLE_ENHANCED_FUNCTIONS 1 << 4
+
 #define THR        0x00 << 3
 #define RHR        0x00 << 3
 #define IER        0x01 << 3
@@ -61,8 +68,9 @@ extern volatile uint8_t I2CSlaveBuffer[BUFSIZE];
 extern volatile uint32_t I2CMasterState;
 extern volatile uint32_t I2CReadLength, I2CWriteLength;
 
-uint32_t write_register(uint8_t reg, uint8_t addr);
-uint8_t read_register(uint8_t reg, uint8_t addr);
+uint32_t write_register(uint8_t reg, uint8_t value);
+uint8_t read_register(uint8_t reg);
+uint32_t uartConnected();
 
 /*******************************************************************************
 **   Main Function  main()
@@ -149,12 +157,55 @@ int main (void)
   // BEGIN WRITE
 
 
-  // Bluetooth
+  // Configure the I2C and UART
 
 
-  // Configure the UART Baud rate
-  write_register(LCR, 0x80);
-  uint8_t f = read_register(LCR, 0x80);
+  write_register(LCR, 0x80); // 0x80 to program baudrate
+  write_register(DLL, DIVL); // ([14.7456 * 10 ^ 6] / 1) / (115200 * 16) = 8 => 0x0008
+  write_register(DLM, DIVM); // The desired baud rate is 115200
+
+  write_register(LCR, 0xBF); // access EFR register
+  //write_register(EFR, EFR_ENABLE_CTS | EFR_ENABLE_RTS | EFR_ENABLE_ENHANCED_FUNCTIONS); // enable enhanced registers
+  write_register(LCR, 0x03); // 8 data bit, 1 stop bit, no parity
+  write_register(FCR, 0x06); // reset TXFIFO, reset RXFIFO, non FIFO mode
+//  write_register(FCR, 0x00); // enable FIFO mode
+//  write_register(IER, 0x01); // enable RHR interrupt
+  //write_register(IOCTRL, 0x01); //
+
+//  if(!uartConnected()){
+//    assert(0);
+//    };
+
+  printf("started\n");
+  fflush(stdout);
+
+  	  for ( i = 0; i < 0x800000; i++ );
+
+  	  printf("finished\n");
+  	  fflush(stdout);
+
+  // Try to read something
+  printf("THR %d\n", read_register(THR));
+  printf("RHR %d\n", read_register(RHR));
+  printf("IER %d\n", read_register(IER));
+  printf("FCR %d\n", read_register(FCR));
+  printf("IIR %d\n", read_register(IIR));
+  printf("LCR %d\n", read_register(LCR));
+  printf("MCR %d\n", read_register(MCR));
+  printf("LSR %d\n", read_register(LSR));
+  printf("MSR %d\n", read_register(MSR));
+  printf("SPR %d\n", read_register(SPR));
+  printf("TXLVL %d\n", read_register(TXLVL));
+  printf("RXLVL %d\n", read_register(RXLVL));
+  printf("DLAB %d\n", read_register(DLAB));
+  printf("IODIR %d\n", read_register(IODIR));
+  printf("IOSTATE %d\n", read_register(IOSTATE));
+  printf("IOINTMSK %d\n", read_register(IOINTMSK));
+  printf("IOCTRL %d\n", read_register(IOCTRL));
+  printf("EFCR %d\n", read_register(EFCR));
+
+
+
   while(1);
 
   I2CWriteLength = 3;
@@ -274,34 +325,50 @@ int main (void)
 }
 
 
-uint32_t write_register(uint8_t reg, uint8_t addr) {
+uint32_t write_register(uint8_t reg, uint8_t value) {
 	  I2CWriteLength = 3;
 	  I2CReadLength = 0;
 	  I2CMasterBuffer[0] = WAADR;
 	  I2CMasterBuffer[1] = reg;
-	  I2CMasterBuffer[2] = addr;
+	  I2CMasterBuffer[2] = value;
 	  I2CEngine();
-	  printf("W reg result: %d\n", I2CMasterState);
+	  //printf("W reg result: %d\n", I2CMasterState);
 	  fflush(stdout);
 
 	  // Assert if the I2C is not in ok mode
 	  assert(I2CMasterState == I2C_OK);
+	  int i = 0 ;
+	  for ( i = 0; i < 0x200000; i++ );
 	  return I2CMasterState;
 }
 
-uint8_t read_register(uint8_t reg, uint8_t addr) {
+uint8_t read_register(uint8_t reg) {
 	  I2CWriteLength = 2;
 	  I2CReadLength = 1;
 	  I2CMasterBuffer[0] = WAADR;
 	  I2CMasterBuffer[1] = reg;
 	  I2CMasterBuffer[2] = RAADR;
 	  I2CEngine();
-	  printf("R reg result: %d\n", I2CMasterState);
+	  //printf("R reg result: %d\n", I2CMasterState);
 	  fflush(stdout);
 
 	  // Assert if the I2C is not in ok mode
 	  assert(I2CMasterState == I2C_OK);
 	  return I2CSlaveBuffer[0];
+}
+
+uint32_t uartConnected() {
+  /*
+
+     Check that UART is connected and operational.
+
+   */
+  // Perform read/write test to check if UART is working
+  const char TEST_CHARACTER = 'H';
+
+  write_register(SPR, TEST_CHARACTER);
+
+  return (read_register(SPR) == TEST_CHARACTER);
 }
 
 /******************************************************************************
