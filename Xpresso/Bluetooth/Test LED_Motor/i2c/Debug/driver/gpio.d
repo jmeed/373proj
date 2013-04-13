7b41d4f9a5bc2f73b500206d0477b67183a41633
jmeed <jmeed@umich.edu>
2013-04-13 17:38:02 -0400

    added and enabled joystick GPIO

--- Xpresso/Bluetooth/Test LED_Motor/i2c/Debug/driver/gpio.d
+++ Xpresso/Bluetooth/Test LED_Motor/i2c/Debug/driver/gpio.d
@@ -1,5 +1,6 @@
 driver/gpio.d: ../driver/gpio.c ../config/driver_config.h \
- ../cmsis/LPC11xx.h ../cmsis/core_cm0.h ../config/system_LPC11xx.h
+ ../cmsis/LPC11xx.h ../cmsis/core_cm0.h ../config/system_LPC11xx.h \
+ ../driver/gpio.h
 
 ../config/driver_config.h:
 
@@ -8,3 +9,5 @@ driver/gpio.d: ../driver/gpio.c ../config/driver_config.h \
 ../cmsis/core_cm0.h:
 
 ../config/system_LPC11xx.h:
+
+../driver/gpio.h:
--- Xpresso/Bluetooth/Test LED_Motor/i2c/Debug/src/globals.d
+++ Xpresso/Bluetooth/Test LED_Motor/i2c/Debug/src/globals.d
@@ -1,1 +1,5 @@
-src/globals.d: ../src/globals.c
+src/globals.d: ../src/globals.c ../src/globals.h ../driver/gpio.h
+
+../src/globals.h:
+
+../driver/gpio.h:
