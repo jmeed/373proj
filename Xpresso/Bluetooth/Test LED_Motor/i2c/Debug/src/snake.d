7c8123f3f8622ba0fb5edeef380de1be7ce152e8
David Jackson <davijack@umich.edu>
2013-04-13 20:00:12 -0400

    hi

--- Xpresso/Bluetooth/Test LED_Motor/i2c/Debug/src/snake.d
+++
@@ -1,5 +0,0 @@
-src/snake.d: ../src/snake.c ../src/snake.h ../src/globals.h
-
-../src/snake.h:
-
-../src/globals.h:
--- Xpresso/Bluetooth/Test LED_Motor/i2c/Debug/src/weather.d
+++
@@ -1,5 +0,0 @@
-src/weather.d: ../src/weather.c ../src/weather.h ../src/globals.h
-
-../src/weather.h:
-
-../src/globals.h:
