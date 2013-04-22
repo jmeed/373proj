66e0cd981f4bf0ebbb8398d0560ea8ec23d3e38a
Filippos Theodorakis <ftheo@umich.edu>
2013-04-22 01:47:55 -0400

    Fixed android app where it is going to wait forever untill it is connected instead of just failing if the watch is not turned on

--- android_final_app/SWatch/src/com/example/swatch/CommThread.java
+++ android_final_app/SWatch/src/com/example/swatch/CommThread.java
@@ -16,7 +16,6 @@ package com.example.swatch;
  * limitations under the License
  */
 
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -33,243 +32,230 @@ import android.os.Build;
 import android.os.Handler;
 import android.util.Log;
 
-
 @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 class CommThread extends Thread {
-    private static BluetoothSocket socket;
-    private static InputStream istream;
-    private static OutputStream ostream;
-    private static Handler handler;
-    private static ProgressDialog dialog;
-    private static BluetoothAdapter adapter;
-    private static byte[] buffer = new byte[1024];
-    
+	private static BluetoothSocket socket;
+	private static InputStream istream;
+	private static OutputStream ostream;
+	private static Handler handler;
+	private static ProgressDialog dialog;
+	private static BluetoothAdapter adapter;
+	private static byte[] buffer = new byte[1024];
 
-    public CommThread(BluetoothAdapter adapter, ProgressDialog dialog, Handler handler) {
-        this.handler = handler;
-        this.dialog = dialog;
-        this.adapter = adapter;
-    }
+	public CommThread(BluetoothAdapter adapter, ProgressDialog dialog,
+			Handler handler) {
+		this.handler = handler;
+		this.dialog = dialog;
+		this.adapter = adapter;
+	}
 
+	public void run() {
+		if (adapter == null)
+			return;
 
-    public void run() {
-                if (adapter == null)
-                        return;
-                
-                Set<BluetoothDevice> devices = adapter.getBondedDevices();
-                BluetoothDevice device = null;
-                for (BluetoothDevice curDevice : devices) {
-                        if (curDevice.getName().matches("RN42-21EE")) {
-                                device = curDevice;
-                                System.out.println("Device found. Breaking!");
-                                break;
-                        }
-                }
-                if (device == null)
-                {
-                        device = adapter.getRemoteDevice("00:06:66:03:A7:52");
-                        System.out.println("Device not found");
-                }
+		Set<BluetoothDevice> devices = adapter.getBondedDevices();
+		BluetoothDevice device = null;
+		for (BluetoothDevice curDevice : devices) {
+			if (curDevice.getName().matches("RN42-21EE")) {
+				device = curDevice;
+				System.out.println("Device found. Breaking!");
+				break;
+			}
+		}
+		if (device == null) {
+			device = adapter.getRemoteDevice("00:06:66:03:A7:52");
+			System.out.println("Device not found");
+		}
 
-                try 
-                {
-                	Method m = null;
-					try {
-						m = device.getClass().getMethod("createRfcommSocket", new Class[] {int.class});
-					} catch (NoSuchMethodException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-                    socket = (BluetoothSocket) m.invoke(device, 1);
-                	
-                        //socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
-                        socket.connect();
-                } catch (IOException e) 
-                {
-                        socket = null;
-                        System.out.println("socket unsuccessfully created");
-                } catch (IllegalArgumentException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
+		while (true) {
+			try {
+				Method m = null;
+				try {
+					m = device.getClass().getMethod("createRfcommSocket",
+							new Class[] { int.class });
+				} catch (NoSuchMethodException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-                if (socket == null) return;
+				socket = (BluetoothSocket) m.invoke(device, 1);
 
-
-        InputStream tmpIn = null;
-        OutputStream tmpOut = null;
-
-
-        try {
-            tmpIn = socket.getInputStream();
-            tmpOut = socket.getOutputStream();
-        } catch (IOException e) { }
-
-
-        istream = tmpIn;
-        ostream = tmpOut;
-        
-        if (dialog != null && dialog.isShowing())
-                dialog.dismiss();
-     try {
-		Thread.sleep(10000);
-	} catch (InterruptedException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	}
-    
-        int act = -1;
-        // Read from Bluetooth always
-        while (true) {
-        	System.out.println("in while");
-        	act = readFromTarget();
-        	handler.obtainMessage(act, buffer)
-            .sendToTarget();
-        }
-    }
-
-    /* Call this from the main Activity to send data to the remote device */
-    public static void write(byte[] bytes) {
-        try {
-            ostream.write(bytes);
-        } catch (IOException e) {
-                Log.e("CommThread.write", "exception during write", e);
-        }
-    }
-
-
-    /* Call this from the main Activity to shutdown the connection */
-    public static void cancel() {
-        try {
-                if (socket != null)
-                        socket.close();
-        } catch (IOException e) { }
-    }
-    
-    public static void ensure_connection()
-    {
-    	
-    	Thread check = new Thread()
-    	{
-    		 @Override
-    	      public void run() {
-    			 while(true)
-    			 {
-    				 if (!socket.isConnected())
-    			    	{
-    			    		System.out.println("socket is not connected");
-    			    		
-    			    	}
-    			 }
-    		 }
-    	};
-    	check.start();
-    }
-    
-    public static void ensure_connection_helper()
-    {
-    	if (socket != null)
-    		return;
-    	else
-    	{
-    		System.out.println("Reconnecting connecting");
-    		if (adapter == null)
-    		{
-    			System.out.println("Adapter is null. Error!");
-    			return;
-    		}
-    	
-        
-        Set<BluetoothDevice> devices = adapter.getBondedDevices();
-        BluetoothDevice device = null;
-        for (BluetoothDevice curDevice : devices) {
-                if (curDevice.getName().matches("RN42-21EE")) {
-                        device = curDevice;
-                        System.out.println("Device found. Breaking!");
-                        break;
-                }
-        }
-        if (device == null)
-        {
-                device = adapter.getRemoteDevice("00:06:66:03:A7:52");
-                System.out.println("Device not found");
-        }
-
-        try 
-        {
-        	Method m = null;
-			try {
-				m = device.getClass().getMethod("createRfcommSocket", new Class[] {int.class});
-			} catch (NoSuchMethodException e) {
+				// socket =
+				// device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+				socket.connect();
+				break;
+			} catch (IOException e) {
+				socket = null;
+				System.out.println("socket unsuccessfully created");
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-            socket = (BluetoothSocket) m.invoke(device, 1);
-                socket.connect();
-        } catch (IOException e) 
-        {
-                socket = null;
-                System.out.println("socket unsuccessfully created");
-        } catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-        if (socket == null) return;
+		if (socket == null)
+			return;
 
+		InputStream tmpIn = null;
+		OutputStream tmpOut = null;
 
-        InputStream tmpIn = null;
-        OutputStream tmpOut = null;
+		try {
+			tmpIn = socket.getInputStream();
+			tmpOut = socket.getOutputStream();
+		} catch (IOException e) {
+		}
 
+		istream = tmpIn;
+		ostream = tmpOut;
 
-        try {
-        	tmpIn = socket.getInputStream();
-        	tmpOut = socket.getOutputStream();
-        } catch (IOException e) { }
+		if (dialog != null && dialog.isShowing())
+			dialog.dismiss();
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
+		int act = -1;
+		// Read from Bluetooth always
+		while (true) {
+			System.out.println("in while");
+			act = readFromTarget();
+			handler.obtainMessage(act, buffer).sendToTarget();
+		}
+	}
 
-        istream = tmpIn;
-        ostream = tmpOut;
+	/* Call this from the main Activity to send data to the remote device */
+	public static void write(byte[] bytes) {
+		try {
+			ostream.write(bytes);
+		} catch (IOException e) {
+			Log.e("CommThread.write", "exception during write", e);
+		}
+	}
 
-        if (dialog != null && dialog.isShowing())
-        	dialog.dismiss();
-    	}
-    	return;
-    }
-    
-    public static int readFromTarget() {
+	/* Call this from the main Activity to shutdown the connection */
+	public static void cancel() {
+		try {
+			if (socket != null)
+				socket.close();
+		} catch (IOException e) {
+		}
+	}
+
+	public static void ensure_connection() {
+
+		Thread check = new Thread() {
+			@Override
+			public void run() {
+				while (true) {
+					if (!socket.isConnected()) {
+						System.out.println("socket is not connected");
+
+					}
+				}
+			}
+		};
+		check.start();
+	}
+
+	public static void ensure_connection_helper() {
+		if (socket != null)
+			return;
+		else {
+			System.out.println("Reconnecting connecting");
+			if (adapter == null) {
+				System.out.println("Adapter is null. Error!");
+				return;
+			}
+
+			Set<BluetoothDevice> devices = adapter.getBondedDevices();
+			BluetoothDevice device = null;
+			for (BluetoothDevice curDevice : devices) {
+				if (curDevice.getName().matches("RN42-21EE")) {
+					device = curDevice;
+					System.out.println("Device found. Breaking!");
+					break;
+				}
+			}
+			if (device == null) {
+				device = adapter.getRemoteDevice("00:06:66:03:A7:52");
+				System.out.println("Device not found");
+			}
+
+			try {
+				Method m = null;
+				try {
+					m = device.getClass().getMethod("createRfcommSocket",
+							new Class[] { int.class });
+				} catch (NoSuchMethodException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				socket = (BluetoothSocket) m.invoke(device, 1);
+				socket.connect();
+			} catch (IOException e) {
+				socket = null;
+				System.out.println("socket unsuccessfully created");
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (socket == null)
+				return;
+
+			InputStream tmpIn = null;
+			OutputStream tmpOut = null;
+
+			try {
+				tmpIn = socket.getInputStream();
+				tmpOut = socket.getOutputStream();
+			} catch (IOException e) {
+			}
+
+			istream = tmpIn;
+			ostream = tmpOut;
+
+			if (dialog != null && dialog.isShowing())
+				dialog.dismiss();
+		}
+		return;
+	}
+
+	public static int readFromTarget() {
 		int activity = -1;
 		buffer[2] = '1';
-		
+
 		int index = 0;
 		byte[] tmp_buff = new byte[1024];
-		while(true) {
+		while (true) {
 			// Receive the sync message to ensure pairing with the right device
 			int bytes = 0; // bytes returned from read()
 			try {
 				bytes = istream.read(tmp_buff);
-				
+
 				// Copy to permenant buffer
 				for (int i = 0; i < bytes; i++) {
 					buffer[index] = tmp_buff[i];
 					index++;
 				}
-				
+
 				// If the last byte is \0 we are done
-				if(buffer[index - 1] == '\0') {
+				if (buffer[index - 1] == '\0') {
 					// Print
 					System.out.print("Just read: ");
-					for(int i = 0; i < index; i++) {
+					for (int i = 0; i < index; i++) {
 						System.out.print((char) buffer[i]);
 					}
 					System.out.println("");
